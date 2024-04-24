8819faf09190bf171d96aed22dc46625c83218a5
Hardik <neopanehardik630@gmail.com>
2024-04-24 21:19:13 +0545

    twelfth commit: login basics

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const login = document.querySelector(".login-form");
+const logbtn = document.querySelector(".log-btn");
+var loggedin = 0;
+
+function lgnclk() {
+  if (loggedin === 0) {
+    login.style.display = "flex";
+  } else if (loggedin === 1) {
+    logbtn.textContent = "Login";
+    loggedin = 0;
+  } else {
+    console.log("Idk");
+  }
+}
+function xclk() {
+  login.style.display = "none";
+}
+document.getElementById("submit-login").onclick = function change() {
+  loggedin = 1;
+  logbtn.textContent = "Logout";
+};
